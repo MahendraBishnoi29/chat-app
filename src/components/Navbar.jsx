373c283f7d6326b58b60ca4463806fe9f56c4db8
Mahendra <mahendrabrowny@gmail.com>
2022-12-25 15:59:30 +0530

    hosting setup done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+
+const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  return (
+    <div className="navbar">
+      <span className="logo">G Chat</span>
+      <div className="user">
+        <img loading="lazy" src={currentUser.photoURL} alt="userPhoto" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={() => signOut(auth)}>logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
